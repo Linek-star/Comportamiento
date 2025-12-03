a3b38ab7bcf08efb83a9746c6c1ca28d4a4bc1b5
Linek-star <elkfuent@espol.edu.ec>
2025-12-02 22:43:04 -0500

    Create Distribuidor.java

---
+++ Strategy/Distribuidor.java
@@ -0,0 +1,26 @@
+package Strategy;
+
+public class Distribuidor {
+
+	Via_Transporte via_de_transporte;
+
+	/**
+	 * 
+	 * @param via
+	 */
+	public void setViaTransporte(Via_Transporte via) {
+		// TODO - implement Distribuidor.setViaTransporte
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param origen
+	 * @param destino
+	 */
+	public Ruta ejecutarDistribucion(String origen, String destino) {
+		// TODO - implement Distribuidor.ejecutarDistribucion
+		throw new UnsupportedOperationException();
+	}
+
+}
