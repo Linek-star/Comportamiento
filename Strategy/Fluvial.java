1d971d9f45061c93064a6e8541184eb5055dc1c6
Linek-star <elkfuent@espol.edu.ec>
2025-12-02 22:43:08 -0500

    Create Fluvial.java

---
+++ Strategy/Fluvial.java
@@ -0,0 +1,10 @@
+package Strategy;
+
+public class Fluvial implements Via_Transporte {
+
+    @Override
+    public Ruta trazar_ruta(String origen, String destino) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
